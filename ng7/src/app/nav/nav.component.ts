ce4bd9c0fe72f8e29da65cd831a46d8fc1f3c921
Victor Johnson <vjohnson22@gmail.com>
2019-09-06 09:58:14 -0400

    added router links

--- ng7/src/app/nav/nav.component.ts
+++ ng7/src/app/nav/nav.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  appTitle: 'myapp'
+  appTitle = 'myapp'
+
   constructor() { }
 
   ngOnInit() {
